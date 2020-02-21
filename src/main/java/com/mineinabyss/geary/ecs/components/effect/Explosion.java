c12425178e92e5440a3514fc4d6486bf38ed0851
Derongan <dgfried@gmail.com>
2020-02-20 21:05:25 -0800

    Add explosion with some settings

--- src/main/java/com/mineinabyss/geary/ecs/components/effect/Explosion.java
+++ src/main/java/com/mineinabyss/geary/ecs/components/effect/Explosion.java
@@ -2,5 +2,24 @@ package com.mineinabyss.geary.ecs.components.effect;
 
 import com.badlogic.ashley.core.Component;
 
+/**
+ * Creates an explosion effect at the target.
+ */
 public class Explosion implements Component {
+
+  public enum Size {
+    SMALL,
+    MEDIUM,
+    LARGE
+  }
+
+  private Size size;
+
+  public Explosion(Size size) {
+    this.size = size;
+  }
+
+  public Size getSize() {
+    return size;
+  }
 }
