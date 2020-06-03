1ee45c8315cef094140546c4cde850b0c24e28c5
Paul1365972 <paul1365972@gmail.com>
2020-06-03 05:58:13 +0200

    Redundant nms (#26)
    * Remove unnecessary NMS usage

--- src/main/java/com/mineinabyss/geary/ecs/system/systems/ProjectileLaunchingSubSystem.java
+++ src/main/java/com/mineinabyss/geary/ecs/system/systems/ProjectileLaunchingSubSystem.java
@@ -6,10 +6,9 @@ import com.mineinabyss.geary.ecs.entity.GearyEntity;
 import com.mineinabyss.geary.ecs.entity.GearyEntityFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_15_R1.entity.CraftSnowball;
-import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
+import org.bukkit.entity.ThrowableProjectile;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -23,19 +22,16 @@ public class ProjectileLaunchingSubSystem {
   }
 
   public GearyEntity launchProjectile(double speed, int projModel, Player player) {
-    org.bukkit.entity.Projectile projectile = player.launchProjectile(Snowball.class,
+    Snowball projectile = player.launchProjectile(Snowball.class,
         player.getEyeLocation().getDirection().normalize()
             .multiply(speed));
-
-    net.minecraft.server.v1_15_R1.EntitySnowball entitysnowball = ((CraftSnowball) projectile)
-        .getHandle();
-
+    
     ItemStack itemStack = new ItemStack(Material.SNOWBALL);
     ItemMeta itemMeta = Bukkit.getItemFactory().getItemMeta(Material.SNOWBALL);
     itemMeta.setCustomModelData(projModel);
     itemStack.setItemMeta(itemMeta);
-
-    entitysnowball.setItem(CraftItemStack.asNMSCopy(itemStack));
+  
+    projectile.setItem(itemStack);
 
     GearyEntity projectileEntity = gearyEntityFactory.createEntity(projectile);
 
